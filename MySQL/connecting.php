647dc556f2beff2a63b6efc0f53a8b282dd53ea4
Simon Phillips <simon.phillips01@gmail.com>
2016-10-28 22:23:09 +0100

    First commit

---
+++ MySQL/connecting.php
@@ -0,0 +1,8 @@
+<?php 
+	
+	$link = mysqli_connect("localhost", "cl20-exampledb", "K7.7grb-Y", "cl20-exampledb");
+
+		if (mysql_connect_error()) {
+			echo ("could not connect to database");
+		}
+?>
---
+++ MySQL/connection.php
@@ -0,0 +1,5 @@
+<?php 
+
+	$link = mysqli_connect ("sql207.bytehost18.com", "b18_15983333", "Pf5Yp8uL", "b18_15983333_diary");
+
+?>
